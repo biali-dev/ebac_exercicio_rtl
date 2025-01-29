d445e208077768c1e4b3d8d4f24a315ec8679ac1
biali <ricardobiali@yahoo.com.br>
2025-01-29 18:59:06 -0300

    comentário

--- src/components/PostComments/__test__/PostComments.test.tsx
+++ src/components/PostComments/__test__/PostComments.test.tsx
@@ -9,28 +9,12 @@ import Post from '..'
 const mock = [
     {
         id: 1,
-        comentario: 'Incrível, eu adorodo os clássicos!'
+        comentario: 'Testando comentário 123'
     },
     {
         id: 2,
-        comentario: 'Eu preciso de um!!! Qual é o valor!?'
+        comentario: 'Segundo teste de comentário'
     },
-    {
-        id: 3,
-        comentario: 'Ele ocupa muito espaço?'
-    },
-    {
-        id: 4,
-        comentario: 'Qual é a loja em que comprou?'
-    },
-    {
-        id: 5,
-        comentario: 'Lindo demais!'
-    },
-    {
-        id: 6,
-        comentario: 'O melhor carro dos quadrinhos!'
-    }
 ]
 
 let contador = 0
@@ -61,7 +45,7 @@ describe('Testes para o componete de comentários', () => {
         fireEvent.change(CaixaDeTexto, {target: {value: mensagem}})
         fireEvent.click(botao)
         //Config do que se espera encontrar no expect
-        expect(screen.getByText('Incrível, eu adorodo os clássicos!')).toBeInTheDocument()
+        expect(screen.getByText('Testando comentário 123')).toBeInTheDocument()
 
         //Ativação do contador e troca para o próximo comentário
         alteraComentario()
@@ -73,50 +57,8 @@ describe('Testes para o componete de comentários', () => {
         const CaixaDeTexto = screen.getByTestId('caixaDeTexto')
         fireEvent.change(CaixaDeTexto, {target: {value: mensagem}})
         fireEvent.click(botao)
-        expect(screen.getByText('Eu preciso de um!!! Qual é o valor!?')).toBeInTheDocument()
+        expect(screen.getByText('Segundo teste de comentário')).toBeInTheDocument()
 
         alteraComentario()
-    })
-
-    test('Deve renderizar o terceiro comentário', async () => {
-        render(<Post />)
-        const botao = screen.getByTestId('btn-comentario')
-        const CaixaDeTexto = screen.getByTestId('caixaDeTexto')
-        fireEvent.change(CaixaDeTexto, {target: {value: mensagem}})
-        fireEvent.click(botao)
-        expect(screen.getByText('Ele ocupa muito espaço?')).toBeInTheDocument()
-
-        alteraComentario()
-    })
-
-    test('Deve renderizar o quarto comentário', async () => {
-        render(<Post />)
-        const botao = screen.getByTestId('btn-comentario')
-        const CaixaDeTexto = screen.getByTestId('caixaDeTexto')
-        fireEvent.change(CaixaDeTexto, {target: {value: mensagem}})
-        fireEvent.click(botao)
-        expect(screen.getByText('Qual é a loja em que comprou?')).toBeInTheDocument()
-
-        alteraComentario()
-    })
-
-    test('Deve renderizar o quinto comentário', async () => {
-        render(<Post />)
-        const botao = screen.getByTestId('btn-comentario')
-        const CaixaDeTexto = screen.getByTestId('caixaDeTexto')
-        fireEvent.change(CaixaDeTexto, {target: {value: mensagem}})
-        fireEvent.click(botao)
-        expect(screen.getByText('Lindo demais!')).toBeInTheDocument()
-
-        alteraComentario()
-    })
-
-    test('Deve renderizar o sexto comentário', async () => {
-        render(<Post />)
-        const botao = screen.getByTestId('btn-comentario')
-        const CaixaDeTexto = screen.getByTestId('caixaDeTexto')
-        fireEvent.change(CaixaDeTexto, {target: {value: mensagem}})
-        fireEvent.click(botao)
-        expect(screen.getByText('O melhor carro dos quadrinhos!')).toBeInTheDocument()
     })
 })
